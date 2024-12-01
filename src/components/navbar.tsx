d3428a30ac04e905b0dcfb157a9c179135b18dc4
auzanassdq <auzanassidqi@gmail.com>
2024-12-01 11:18:27 +0700

    create navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
-    <section className='border'>
-
-    <header className='container p-4 mx-auto flex justify-between items-center'>
-      <div className="logo">
-        {/* <img src="/logo.png" alt="logo" /> */}
-        <h1 className='text-4xl font-bold indie-flower'>Makasih</h1>
-      </div>
-      <nav>
-        <ul className='flex gap-4'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Support</a></li>
-        </ul>
-      </nav>
-    </header>
+    <section className="border">
+      <header className="container mx-auto flex items-center justify-between p-4">
+        <div className="logo">
+          {/* <img src="/logo.png" alt="logo" /> */}
+          <Link href={"/"}>
+          <h1 className="indie-flower text-4xl font-bold">Makasih</h1>
+          </Link>
+        </div>
+        <nav className="flex gap-4">
+          <Link href="/story" className="hover:underline">Kisah mereka</Link>
+          <Link href="/support" className="hover:underline">Support</Link>
+        </nav>
+      </header>
     </section>
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
