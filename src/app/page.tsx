13815fd5dbfaf89f0221a24205535efe6031bf25
auzanassdq <auzanassidqi@gmail.com>
2024-12-01 11:21:22 +0700

    calling hero and story section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
+import CommunityStory from "@/components/community-story";
+import Footer from "@/components/footer";
 import Hero from "@/components/hero";
+import Howitwork from "@/components/howitwork";
 import Navbar from "@/components/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Hero />
-      
+      {/* <Howitwork/> */}
+      <CommunityStory />
     </div>
   );
 }
