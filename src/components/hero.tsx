a86eaeee79dd89e779738298284fdc3abb977a9f
auzanassdq <auzanassidqi@gmail.com>
2024-12-01 11:18:46 +0700

    create hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 function Hero() {
   return (
-    <section className="text-center py-56">
-      <div className="">
-        <h1 className="text-6xl font-bold indie-flower">
+    <section className="py-24 overflow-x-clip">
+      <div className="text-center">
+        <h1 className="indie-flower text-5xl font-bold md:text-6xl">
           Sudah bersyukur hari ini ?
         </h1>
         <p className="mt-4 text-lg text-neutral-500/80">
-          yuk mulai mensyukuri dari hal-hal yg paling kecil
+          Tulis rasa syukurmu hari ini, dan biarkan dunia melihat keindahannya.
         </p>
         <div className="mt-10 flex justify-center gap-4">
-          <Button>Tulis di sini</Button>
+          <Link href={"/post"}>
+            <Button>Tulis di sini</Button>
+          </Link>
           <Button variant={"secondary"}>lihat yg lain</Button>
         </div>
       </div>
