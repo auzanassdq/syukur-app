a0973351c15e45e4c60a2a10c05954a2852ca9b6
auzanassdq <auzanassidqi@gmail.com>
2024-12-01 11:18:55 +0700

    craete footer section

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-center my-10'>
+      <p className='text-black/50'>"Hal-hal kecil yang kita syukuri adalah jembatan menuju kebahagiaan besar."</p>
+      <p className='text-black/50'>&copy; 2024 Makasih. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
